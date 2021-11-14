65e0c696fd28704995022793687df79ba95c256b
felipprodrigues <dev.rodriguesfelipp@gmail.com>
2021-11-14 18:55:21 -0300

    include initial files and dependencies

---
+++ pws/src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../scss/main.css';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="catch-of-the-day">
+        <p>Hello World</p>
+      </div>
+    )
+  }
+}
+
+export default App;
